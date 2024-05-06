69a3664bab314f878d7beb231da0c355e27c921a
zak <zaqueoae@gmail.com>
2024-05-06 11:57:37 +0200

    Create comandos_personalizadosarch.sh

---
+++ comandos_personalizadosarch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+> ~/.bashrc
+#Permisos al servidor local
+echo '
+    [[ $- != *i* ]] && return
+
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    PS1='[\u@\h \W]\$ '
+    
+    # kitty ssh fix
+    [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+    #Permisos servidor local
+    pw() {
+      sudo mkdir -p /srv/http
+      sudo mkdir -p /var/repositorio
+      sudo mkdir -p /var/create_tpv
+      sudo chown -R "$currentuser":http /srv/http
+      sudo chown -R "$currentuser":http /var/repositorio
+      sudo chown -R "$currentuser":http /var/create_tpv
+      sudo chmod -R g+w /var/repositorio
+      sudo chmod -R g+w /var/create_tpv
+      sudo chmod -R g+w /srv/http
+    }
+' >> ~/.bashrc
+
+source ~/.bashrc
+
+
+
+
+
+
+
+# Recarga .bashrc
+source ~/.bashrc
